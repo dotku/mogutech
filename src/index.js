92115cfd3a97f82cdf84487b649db489e75afb87
Weijing Jay Lin <weijingjaylin@gmail.com>
2022-02-11 14:47:06 -0800

    improve policy and the home page

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
+document.forms[0].onsubmit = (e) => {
+  e.preventDefault();
+  const subject = document.getElementById("subject").value;
+  const content = document.getElementById("content").value;
+
+  window.open(
+    `mailto:weijingjaylin@gmail.com?subject=${subject}&body=${content}`
+  );
+};
 // function component() {
 //   const element = document.createElement("div");
 
@@ -8,5 +17,3 @@
 // }
 
 // document.body.appendChild(component());
-
-console.log("hello");
